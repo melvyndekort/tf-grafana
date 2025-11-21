54bdd2c1f0ba438786e6f24042b4c46c826c8a92
Melvyn de Kort <melvyn@mdekort.nl>
2025-11-22 00:26:42 +0100

    Add alerting permissions

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -5,10 +5,26 @@ data "grafana_role" "dashboards_writer" {
   depends_on = [grafana_cloud_stack_service_account_token.terraform]
 }
 
+data "grafana_role" "alerting_writer" {
+  provider = grafana.mdekort
+  name     = "fixed:alerting:writer"
+
+  depends_on = [grafana_cloud_stack_service_account_token.terraform]
+}
+
 resource "grafana_role_assignment" "email_infra_dashboard_writer" {
   provider = grafana.mdekort
 
   role_uid         = data.grafana_role.dashboards_writer.uid
+  service_accounts = [grafana_cloud_stack_service_account.email_infra.id]
+
+  depends_on = [grafana_cloud_stack_service_account_token.terraform]
+}
+
+resource "grafana_role_assignment" "email_infra_alerting_writer" {
+  provider = grafana.mdekort
+
+  role_uid         = data.grafana_role.alerting_writer.uid
   service_accounts = [grafana_cloud_stack_service_account.email_infra.id]
 
   depends_on = [grafana_cloud_stack_service_account_token.terraform]
