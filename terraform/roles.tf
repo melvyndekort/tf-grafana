20528ae44b1807aad4ad3f2d53cc3bde8c20e32f
Melvyn de Kort <melvyn@mdekort.nl>
2025-11-25 22:47:28 +0100

    Add datasources:reader role for email-infra service account

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -1,1 +1,16 @@
 
+data "grafana_role" "datasources_reader" {
+  provider = grafana.mdekort
+  name     = "fixed:datasources:reader"
+
+  depends_on = [grafana_cloud_stack_service_account_token.terraform]
+}
+
+resource "grafana_role_assignment" "email_infra_datasources_reader" {
+  provider = grafana.mdekort
+
+  role_uid         = data.grafana_role.datasources_reader.uid
+  service_accounts = [grafana_cloud_stack_service_account.email_infra.id]
+
+  depends_on = [grafana_cloud_stack_service_account_token.terraform]
+}
